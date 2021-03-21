31a689e6d9f83a8d1c715bc8d696f765c1b5e4b4
Stefan Nede <stefan@Stefans-Air.home>
2021-03-21 10:23:21 +0000

    commit on 'main'

--- dist/projects.js
+++ dist/projects.js
@@ -1,1 +1,11 @@
 "use strict";
+const hamburger = document.querySelector(".hamburger");
+const hamburger_lines = document.querySelectorAll(".hl");
+const nav = document.querySelector(".main_nav");
+hamburger.addEventListener("click", () => {
+    hamburger_lines[1].classList.toggle("clicked");
+    hamburger_lines[3].classList.toggle("clicked");
+    hamburger_lines[0].classList.toggle("dissapear");
+    hamburger_lines[2].classList.toggle("dissapear");
+    nav.classList.toggle("clear");
+});
