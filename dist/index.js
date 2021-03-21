379250bb7ec73f55a7a99ef2fa46d5bd51c66ea4
Stefan Nede <stefan@Stefans-Air.home>
2021-03-21 12:34:43 +0000

    commit on 'main'

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,12 @@
 const hamburger = document.querySelector(".hamburger");
 const hamburger_lines = document.querySelectorAll(".hl");
 const nav = document.querySelector(".main_nav");
+const content = document.querySelector(".rest_of_page");
 hamburger.addEventListener("click", () => {
     hamburger_lines[1].classList.toggle("clicked");
     hamburger_lines[3].classList.toggle("clicked");
     hamburger_lines[0].classList.toggle("dissapear");
     hamburger_lines[2].classList.toggle("dissapear");
+    content.classList.toggle("nav_open");
     nav.classList.toggle("clear");
 });
--- ts/index.ts
+++ ts/index.ts
@@ -1,11 +1,14 @@
 const hamburger:any = document.querySelector(".hamburger");
 const hamburger_lines:any = document.querySelectorAll(".hl");
 const nav:any = document.querySelector(".main_nav");
+const content:any = document.querySelector(".rest_of_page");
+
 
 hamburger.addEventListener("click", ()=>{
     hamburger_lines[1].classList.toggle("clicked");
     hamburger_lines[3].classList.toggle("clicked");
     hamburger_lines[0].classList.toggle("dissapear");
     hamburger_lines[2].classList.toggle("dissapear");
+    content.classList.toggle("nav_open");
     nav.classList.toggle("clear");
 });
