c6c05190e2df6d92bc61a4d140772866255a4b5b
Stefan Nedelcu <74429608+StefanNede@users.noreply.github.com>
2021-03-25 22:53:59 +0000

    Add files via upload

--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,17 @@ const content = document.querySelector(".rest_of_page");
 const scrollDown = document.querySelector(".scroll-down");
 const aboutMeContent = document.querySelector(".appear-further-down");
 const customCursor = document.querySelector(".circle");
+// back to top button coding
+const backToTopButton = document.querySelector(".back-to-top");
+backToTopButton === null || backToTopButton === void 0 ? void 0 : backToTopButton.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+});
 // this gets the coordinates of the about me content for the smooth scrolling button
 // you can use top, bottom, left, and right
-const positions = aboutMeContent.getBoundingClientRect();
+const positions = aboutMeContent === null || aboutMeContent === void 0 ? void 0 : aboutMeContent.getBoundingClientRect();
 const hello = "hello";
 document.addEventListener("mousemove", function (event) {
-    const customCursorPositions = customCursor.getBoundingClientRect();
+    const customCursorPositions = customCursor === null || customCursor === void 0 ? void 0 : customCursor.getBoundingClientRect();
     let cursorHeight = (customCursorPositions.height);
     let cursorWidth = (customCursorPositions.width);
     let xpos = ((event.pageX - cursorWidth / 2)).toString();
@@ -24,10 +29,10 @@ hamburger.addEventListener("click", () => {
     hamburger_lines[3].classList.toggle("clicked");
     hamburger_lines[0].classList.toggle("dissapear");
     hamburger_lines[2].classList.toggle("dissapear");
-    content.classList.toggle("nav_open");
+    content === null || content === void 0 ? void 0 : content.classList.toggle("nav_open");
     nav.classList.toggle("clear");
 });
 // scroll to the top of the about me content
-scrollDown.addEventListener("click", () => {
+scrollDown === null || scrollDown === void 0 ? void 0 : scrollDown.addEventListener("click", () => {
     window.scrollTo(0, positions.top);
 });
--- ts/index.ts
+++ ts/index.ts
@@ -6,13 +6,20 @@ const scrollDown:HTMLButtonElement|null = document.querySelector(".scroll-down")
 const aboutMeContent:HTMLElement|null = document.querySelector(".appear-further-down");
 const customCursor:HTMLSpanElement|null = document.querySelector(".circle");
 
+// back to top button coding
+const backToTopButton:HTMLButtonElement|null = document.querySelector(".back-to-top");
+
+backToTopButton?.addEventListener("click", ()=>{
+    window.scrollTo(0, 0);
+});
+
 // this gets the coordinates of the about me content for the smooth scrolling button
 // you can use top, bottom, left, and right
-const positions = aboutMeContent.getBoundingClientRect();
+const positions = aboutMeContent?.getBoundingClientRect();
 const hello:string = "hello";
 
 document.addEventListener("mousemove", function(event){
-    const customCursorPositions:Object = customCursor.getBoundingClientRect();
+    const customCursorPositions:Object = customCursor?.getBoundingClientRect();
     let cursorHeight:number = (customCursorPositions.height);
     let cursorWidth:number = (customCursorPositions.width);
     let xpos:string = ((event.pageX - cursorWidth/2)).toString();
@@ -27,11 +34,11 @@ hamburger.addEventListener("click", ()=>{
     hamburger_lines[3].classList.toggle("clicked");
     hamburger_lines[0].classList.toggle("dissapear");
     hamburger_lines[2].classList.toggle("dissapear");
-    content.classList.toggle("nav_open");
+    content?.classList.toggle("nav_open");
     nav.classList.toggle("clear");
 });
 
 // scroll to the top of the about me content
-scrollDown.addEventListener("click", ()=>{
+scrollDown?.addEventListener("click", ()=>{
     window.scrollTo(0, positions.top);
 });
